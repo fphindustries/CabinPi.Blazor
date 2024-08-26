5efb319debfd8503b324eaed36d276847710ac40
SingleBarrel <cpkent@gmail.com>
2024-08-25 19:15:15 -0700

    chore: Update MainLayout.razor.css and ExtensionMethods.cs

--- CabinPi.Blazor/Models/Measurement.cs
+++ CabinPi.Blazor/Models/Measurement.cs
@@ -8,31 +8,31 @@ namespace CabinPi.Blazor.Models
     public class Measurement
     {
         public DateTime Date { get; set; }
-        public int AbsorbTime { get; set; }
-        public int AmpHours { get; set; }
-        public int EqualizeTime { get; set; }
-        public int FloatTime { get; set; }
-        public float HighestVinputLog { get; set; }
-        public float IbattDisplay { get; set; }
-        public int NiteMinutesNoPwr { get; set; }
-        public float PvInputCurrent { get; set; }
-        public float VocLastMeasured { get; set; }
-        public int BatteryState { get; set; }
-        public int ChargeState { get; set; }
-        public int ClassicState { get; set; }
-        public float DispavgVbatt { get; set; }
-        public float DispavgVpv { get; set; }
-        public float kWHours { get; set; }
-        public int Watts { get; set; }
-        public float Case_C { get; set; }
-        public float Case_F { get; set; }
-        public float Ext_C { get; set; }
-        public float Ext_F { get; set; }
-        public float hPa { get; set; }
-        public float Humidity { get; set; }
-        public float inHg { get; set; }
-        public float Int_C { get; set; }
-        public float Int_F { get; set; }
+        public int? AbsorbTime { get; set; }
+        public int? AmpHours { get; set; }
+        public int? EqualizeTime { get; set; }
+        public int? FloatTime { get; set; }
+        public float? HighestVinputLog { get; set; }
+        public float? IbattDisplay { get; set; }
+        public int? NiteMinutesNoPwr { get; set; }
+        public float? PvInputCurrent { get; set; }
+        public float? VocLastMeasured { get; set; }
+        public int? BatteryState { get; set; }
+        public int? ChargeState { get; set; }
+        public int? ClassicState { get; set; }
+        public float? DispavgVbatt { get; set; }
+        public float? DispavgVpv { get; set; }
+        public float? kWHours { get; set; }
+        public int? Watts { get; set; }
+        public float? Case_C { get; set; }
+        public float? Case_F { get; set; }
+        public float? Ext_C { get; set; }
+        public float? Ext_F { get; set; }
+        public float? hPa { get; set; }
+        public float? Humidity { get; set; }
+        public float? inHg { get; set; }
+        public float? Int_C { get; set; }
+        public float? Int_F { get; set; }
         public bool? InverterOn { get; set; }
         public int? InverterMode { get; set; }
         public int? InverterFault { get; set; }
